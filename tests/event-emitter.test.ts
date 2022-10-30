a31edd2b2a6edf8877fa4127cd2049ae2c4e9534
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2022-10-30 13:42:44 -0300

    feat: tests for event-emitter

---
+++ tests/event-emitter.test.ts
@@ -0,0 +1,64 @@
+import { describe, expect, it } from "@jest/globals";
+import { EventEmitter } from "../lib/modules/event-emmiter";
+import { TriggersOn } from "../lib/modules/event-emmiter/decoratos/triggers-on";
+
+describe("Event Emitter", () => {
+  const eventNames = ["event1", "event2"];
+
+  const createEventEmitter = (events?: string[]) => {
+    const eventEmitter = new EventEmitter(events?.length ? events : eventNames);
+    return eventEmitter;
+  };
+
+  it("Should init a event-emitter class", () => {
+    const eventEmitter = createEventEmitter();
+
+    expect(eventEmitter).toBeInstanceOf(EventEmitter);
+    expect(eventEmitter.events[eventNames[0]]).toEqual([]);
+  });
+
+  it("Should trigger on an event", async () => {
+    const eventEmitter = createEventEmitter();
+    const eventName = eventNames[0];
+
+    jest.spyOn(EventEmitter.prototype, "on");
+    expect(EventEmitter.prototype.on).toBeCalledTimes(0);
+
+    class TestClass {
+      @TriggersOn(eventName, eventEmitter)
+      triggersOnTest() {}
+    }
+    new TestClass();
+
+    expect(EventEmitter.prototype.on).toBeCalledTimes(1);
+  });
+
+  it("Should emit an event", async () => {
+    const eventEmitter = createEventEmitter();
+    const eventName = eventNames[0];
+
+    jest.spyOn(EventEmitter.prototype, "on");
+    expect(eventEmitter.events[eventName].length).toBe(0);
+
+    class TestClass {
+      @TriggersOn(eventName, eventEmitter)
+      triggersOnTest() {}
+    }
+    new TestClass();
+
+    await eventEmitter.emit(eventName, { payload: "payload" });
+    expect(eventEmitter.events[eventName].length).toBe(1);
+  });
+
+  it("Should not emit an inexistent event", async () => {
+    const eventEmitter = createEventEmitter();
+    const eventName = "wrong-event-name";
+
+    expect(() => {
+      class TestClass {
+        @TriggersOn(eventName, eventEmitter)
+        triggersOnTest() {}
+      }
+    }).toThrow();
+  });
+});
